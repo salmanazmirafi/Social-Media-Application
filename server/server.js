ff373a07da539773ebf3435cf2ae27b8ccc7fcf9
Salman Azmi Rafi <salmanazmirafipersonal@gmail.com>
2022-11-29 22:40:48 +0600

    Update

--- server/server.js
+++ server/server.js
@@ -24,4 +24,4 @@ mongoose.connect(process.env.DATABASE_URI, () => {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on port ${port} 🔥 🙂 `));
+app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
